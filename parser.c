c982ebd6cd0db7368972ee087091f7f874426e7e
Griiis23 <73113520+Griiis23@users.noreply.github.com>
2020-10-19 17:18:50 +0200

    Update parser.c

--- parser.c
+++ parser.c
@@ -3,7 +3,7 @@
 /*
     Fichier parcer.c : Implémentation des fonctions de traitement de
                        la ligne de commande
-    Groupe : n° X
+    Groupe : n° 17
     Auteur :
     Dépendances :
 */
@@ -16,6 +16,20 @@
       Retourne 0 en cas de succés, une autre valeur en cas d'échec
  */
 int trim_str(char* str) {
+    int i=0;
+    int len = strlen(str);
+    if(len > MAX_LINE_SIZE) return 1;
+    
+    //On compte le nombre de blanc au debut
+    while(isblank(str[i])) i++;
+    //On deplace i caractère vers la gauche
+    memmove(str,str+i,len-i+1);
+    
+    //On remplace tous les blancs par des \0 a la fin
+    i=strlen(str)-1;
+    while(isblank(str[i])) str[i--]='\0';
+    
+    return 0;
 
 }
 
@@ -25,8 +39,20 @@ int trim_str(char* str) {
       Paramètre str : la chaîne à modifier
       Retourne 0 en cas de succés, une autre valeur en cas d'échec
  */
-int clean_str(char* str) {
-
+int clean_str(char* str) { 
+    char new_str[MAX_LINE_SIZE];
+	for(int i=0; i<strlen(str);i++){
+	    //On récupère les premiers exemplaires de chaque caractère
+	   	new_str[i]=str[i];
+	   	//On passe à l'élément suivant lorsqu'on trouve des doublons d'espaces 
+	   	// ou de \t  
+        if(str[i] == ' ' || str[i] == '\t'){
+            if(str[i] == str[i+1]){
+				i++;
+	    	}
+		}
+	}
+    return 0;
 }
 
 /*
@@ -38,7 +64,18 @@ int clean_str(char* str) {
       Retourne le nombre de chaînes dans tokens
  */
 int tokenize_str(char* str, char* tokens[]) {
-
+    int i = 0;
+    // On récupère le 1er token
+    char * token = strtok ( str, "\t " );
+    while ( token != NULL ) {
+        // On le copie dans tokens
+        printf("%s",token);
+        //strcpy(tokens[i], token);
+        i++;
+        // On récupère le prochain token
+        token = strtok ( NULL, "\t " );
+    }
+    return i;
 }
 
 /*
@@ -48,5 +85,18 @@ int tokenize_str(char* str, char* tokens[]) {
       Retourne 0 en cas de succés, une autre valeur en cas d'échec
  */
 int env_str(char* tokens[]) {
+    return 0;
+}
 
+
+
+
+#include <stdlib.h>
+
+int main(int argc, char* argv[]){
+    //char oui[10][10];
+    char oui2[] = "ls oui -l";
+    tokenize_str(oui2,NULL);
+    
+    return 0;
 }
